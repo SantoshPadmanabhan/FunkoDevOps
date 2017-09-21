e6dde0e9eab483bec129c84a2724044e6db39a4b
Santosh Padmanabhan <santosh.padmanabhan@brillio.com>
2017-09-21 22:41:34 +0530

    Add project files.

---
+++ FunkoDevOps/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FunkoDevOps.Startup))]
+namespace FunkoDevOps
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
